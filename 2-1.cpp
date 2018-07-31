ab853a21fbd3ab1448225126f006ac26c71cf4f7
samsung-PC\samsung <2jy22@naver.com>
2018-07-31 20:56:12 +0900

    no message

---
+++ 2-1.cpp
@@ -0,0 +1,16 @@
+// cout과 << 연산자를 이용하여, 1에서 100까지 정수를 다음과 같이 한 줄에 10개씩 출력하라.
+// 각 정수는 탭으로 분리하여 출력하라
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	for (int i = 0; i < 100; i++) {
+		cout << i + 1 << '\t';
+		if ((i + 1) % 10 == 0) {
+			cout << endl;
+		}
+	}
+
+	return 0;
+}
---
+++ 2-2.cpp
@@ -0,0 +1,20 @@
+// cout과 << 연산자를 이용하여 다음과 같이 구구단을 출력하는 프로그램을 작성하라.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int result;
+
+	for (int i = 1; i < 10; i++) {
+		for (int j = 1; j < 10; j++) {
+			result = j * i;
+			cout << j << 'x' << i << '=' << result << '\t';
+			if (j == 9) {
+				cout << endl;
+			}
+		}
+	}
+
+	return 0;
+}
